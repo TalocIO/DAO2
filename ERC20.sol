0506305a2b8d0ae92693a45272d924cbdec95f61
Francisco <frankyautran@gmail.com>
2022-07-28 04:12:41 -0700

    test

--- ERC20.sol
+++ ERC20.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+//test
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
