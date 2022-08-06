0c350e6af6e637cf500b585c0d4c0f6434640158
ftank90 <103961034+ftank90@users.noreply.github.com>
2022-08-05 19:26:10 -0700

    added new contracts

---
+++ contracts/Taloc/registration.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.3;
+
+contract registration () {
+
+    constructor() {
+
+    }; 
+
+    function acceptance (struct KYC_status, string user_location) { 
+
+    }; 
+
+    function registration ( bool discord_reg, address wallet_ID ) { 
+
+    };
+
