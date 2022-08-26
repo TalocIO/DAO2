a245baa7b58fbc3727922533c5f2bc608c8242b9
ftank90 <103961034+ftank90@users.noreply.github.com>
2022-08-26 03:00:46 -0700

    changes

--- contracts/Taloc/Voting.sol
+++ contracts/Taloc/Voting.sol
@@ -19,29 +19,43 @@ CREATE { purchase_ID }
 AND COUNT
 { user_location }
 
-FROM { KYC }
+FROM { d_storage }
 
     }; 
 
-    function voting_eligibility (bool _subscribed , string majority_location) private returns() {
 
-    };
-
-//will this be a function or event ? will be triggered by "voting_eligibility" function ^^ 
+//will this be a function or event ? will be triggered by "voting_eligibility" function 
 //and notify eligible members to vote. 
 //how will members be notified ? A = discord, will need to integrate thier API with this function 
 
     function member_notify ( uint256 Purchase_ID , uint256 amount_raised,
     string _majority_location, uint256 voting_timelock) private returns() {
 
-    }; 
+    };
 
-
-//this function will need to be scheduled (timelock ? ) need a start and end time  
-
-    function vote (uint256 voting_timelock, uint256 Purchase_ID) private returns() {
+    function voting_eligibility (bool _registration , string majority_location) private returns() {
+        //during location count, will need to omit previous location winner, 
+        //example: if last purchase was in LA, and LA is majority again, will need to go to second most city 
 
     };
 
+
+     function member_vote () private returns() {
+
+    };
+
+
+
+
+
+    function vote (uint256 voting_timelock, uint256 Purchase_ID) private returns() {
+//this function will need to be scheduled (timelock ? ) need a start and end time  
+
+    };
+
+
+    function voters_decision (uint256 voting_timelock, uint256 Purchase_ID) private returns() {
+    //will tally the votes and pass decision to purchase function 
+    };
     
 }
--- contracts/Taloc/registration.sol
+++ contracts/Taloc/registration.sol
@@ -1,31 +1,89 @@
 // SPDX-License-Identifier: MIT
 
+//need to import KYC oracle here
+
+d_storage = 'conenction' 
+
 pragma solidity ^0.7.3;
 
-contract registration () {
-
-    constructor() {
-
-    }; 
-
-    //sudo code, need to finalize logic 
-    function location_validator () pure return (bool memory) {
-
-//User must be in location defined as:
-//majority minority demographic
-//low income
- index d_storage for match 
-
-   return location_validator; };
+contract registration () { 
 
 
-    function acceptance (struct KYC_status, string user_location) private returns() { 
+        constructor() {
 
     }; 
 
-    function registration ( bool discord_reg, address wallet_ID ) private returns()  { 
+    function create_member () returns (bool member_created) {
+        uint256 member_ID = ;
+        address wallet_address = ;
+        string KYC_status = ["pending", "approved", "declined"];
+        struct user_location = [string state = "CA" , uint256 zip_code = "91342"];
+        string member_status = ["pending", "approved", "declined"];
+        bool member_registered = ["true", "false"];
+        bool member_created = ["true", "false"] ;
 
-    };
+
+//will be listening for event on the FE, when user clicks "join" button 
+//will create a "member" struct and auto generate a member_ID - with the following: 
+
+    struct member [ member_ID, wallet_address, KYC_status = 'pending' , member_status = 'pending', 
+    member_registered = false, user_location [state, zip_code]]
+
+    return member_created; }; 
+    
+
+
+    function location_validator (string user_location) pure returns (bool memory location_validated) {
+        bool location_validated = ["true", "false"];
+        struct user_location = ["state", "zip_code"]
+
+        //listening for event "KYC_status' updated from 'pending' to 'approved'
+        event.listening ()
+
+        index 'user_location' FROM 'd_storage'  for a match against 'accpeted_locations'  
+
+        if 'user_location' = 'accepeted_location' 
+            then location_validated = true
+            else location_validated = false;
+
+        return location_validated; 
+
+            
+            if location_validated = true 
+                 then write to 'd_storage' update 'match' = YES
+                 else };
+
+
+
+
+    function acceptance (string KYC_status, bool location_validated) private 
+        returns(bool member_approved) {
+            bool member_approved =  ["true", "false"];
+
+            //listening for event, what triggers this ? 
+             event.listening ()
+
+        return member_approved ;
+        
+            if member_approved = true 
+            then write to 'd_storage' update 'member_status' = approved
+            else  ; }; 
+
+
+
+
+    function registration ( bool discord_reg, address wallet_address, bool member_approved ) private 
+        returns(bool registration_complete)  { 
+            bool registration_complete = ["true", "false"];
+
+            //listening for event, what triggers this ? 
+             event.listening ()
+
+            return registration_complete ;
+
+                 if registration_comeplete = true 
+                 then write to 'd_storage' update 'member_registered' = true
+                 else ; };
 
 };
 
