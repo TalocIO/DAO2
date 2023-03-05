fb92ed0ce4e3969f6c2a5ff32131bb2adb8f1646
ftank90 <103961034+ftank90@users.noreply.github.com>
2023-03-04 20:12:57 -0800

    added multi step to profile

--- my-app3/src/pages/profile/index.jsx
+++ my-app3/src/pages/profile/index.jsx
@@ -1,137 +1,34 @@
 import React from 'react'
 import { Box, Button, TextField } from '@mui/material'
-import { Formik } from "formik";
-import * as yup from 'yup';
-import { useMediaQuery } from "@mui/material";
 import Header from "../../components/Header";
+import MultiStep from "react-multistep";
+import { StepOne } from "/Applications/GitHub/DAO2/my-app3/src/components/step-one.jsx";
+import { StepTwo } from "/Applications/GitHub/DAO2/my-app3/src/components/step-two.jsx";
+import { StepThree } from "/Applications/GitHub/DAO2/my-app3/src/components/step-three.jsx";
+import { StepFour } from "/Applications/GitHub/DAO2/my-app3/src/components/step-four.jsx";
+
+import '/Applications/GitHub/DAO2/my-app3/src/app.css'
 
 const Profile = () => {
-    const isNonMobile = useMediaQuery("(min-width:600px)");
-    const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-    const handleFormSubmit = (values) => {
-        console.log(values);
-    };
-    const initialValues = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        contact: "",
-        address1: "",
-        address2: "",
-    };
-    const checkoutSchema = yup.object().shape({
-        firstName:yup.string().required("Required"),
-        lastName:yup.string().required("Required"),
-        email:yup.string().email("Invalid email!").required("Required"),
-        contact:yup.string().matches(phoneRegExp, "phone number is not valid!").required("Required"),
-        address1:yup.string().required("Required"),
-        address2:yup.string().required("Required"),
-
-    })
-
-
+  
+  const steps = [
+    { name: "Name A", component: <StepOne /> },
+    { name: "Email", component: <StepTwo /> },
+    { name: "Password", component: <StepThree /> },
+    { name: "Agreement", component: <StepFour /> }
+  ];
+    
+    
     return (
         <Box m="20px">
-          <Header title="CREATE USER" subtitle="Create a New User Profile" />
+          <Header title="Member Profile" subtitle="Your Information" />
     
-          <Formik onSubmit={handleFormSubmit} initialValues={initialValues} validationSchema={checkoutSchema}>
-            {({ values, errors, touched, handleBlur, handleChange, handleSubmit,}) => (
-              <form onSubmit={handleSubmit}>
                 <Box
-                  display="grid"
-                  gap="30px"
-                  gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-                  sx={{
-                    "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-                  }}
-                >
-                  <TextField
-                    fullWidth
-                    variant="filled"
-                    type="text"
-                    label="First Name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.firstName}
-                    name="firstName"
-                    error={!!touched.firstName && !!errors.firstName}
-                    helperText={touched.firstName && errors.firstName}
-                    sx={{ gridColumn: "span 2" }}
-                  />
-                  <TextField
-                    fullWidth
-                    variant="filled"
-                    type="text"
-                    label="Last Name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.lastName}
-                    name="lastName"
-                    error={!!touched.lastName && !!errors.lastName}
-                    helperText={touched.lastName && errors.lastName}
-                    sx={{ gridColumn: "span 2" }}
-                  />
-                  <TextField
-                    fullWidth
-                    variant="filled"
-                    type="text"
-                    label="Email"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.email}
-                    name="email"
-                    error={!!touched.email && !!errors.email}
-                    helperText={touched.email && errors.email}
-                    sx={{ gridColumn: "span 4" }}
-                  />
-                  <TextField
-                    fullWidth
-                    variant="filled"
-                    type="text"
-                    label="Contact Number"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.contact}
-                    name="contact"
-                    error={!!touched.contact && !!errors.contact}
-                    helperText={touched.contact && errors.contact}
-                    sx={{ gridColumn: "span 4" }}
-                  />
-                  <TextField
-                    fullWidth
-                    variant="filled"
-                    type="text"
-                    label="Address 1"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.address1}
-                    name="address1"
-                    error={!!touched.address1 && !!errors.address1}
-                    helperText={touched.address1 && errors.address1}
-                    sx={{ gridColumn: "span 4" }}
-                  />
-                  <TextField
-                    fullWidth
-                    variant="filled"
-                    type="text"
-                    label="Address 2"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.address2}
-                    name="address2"
-                    error={!!touched.address2 && !!errors.address2}
-                    helperText={touched.address2 && errors.address2}
-                    sx={{ gridColumn: "span 4" }}
-                  />
-                </Box>
-                <Box display="flex" justifyContent="end" mt="20px">
-                  <Button type="submit" color="secondary" variant="contained">
-                    Create New User
-                  </Button>
-                </Box>
-              </form>
-            )}
-          </Formik>
+                   sx={{ width: '100%' }}
+                  >                
+                  <MultiStep steps={steps} /> 
+                  
+                </Box>          
         </Box>
       );
 }
