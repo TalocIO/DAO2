690a616b0fde8fa06577e024b3fd9f47b3dd0ed7
ftank90 <103961034+ftank90@users.noreply.github.com>
2023-04-12 19:38:41 -0700

    removed build

--- my-app3/src/pages/profile/index.jsx
+++ my-app3/src/pages/profile/index.jsx
@@ -5,17 +5,17 @@ import MultiStep from "react-multistep";
 import { StepOne } from "/Applications/GitHub/DAO2/my-app3/src/components/step-one.jsx";
 import { StepTwo } from "/Applications/GitHub/DAO2/my-app3/src/components/step-two.jsx";
 import { StepThree } from "/Applications/GitHub/DAO2/my-app3/src/components/step-three.jsx";
-import { StepFour } from "/Applications/GitHub/DAO2/my-app3/src/components/step-four.jsx";
+//import { StepFour } from "/Applications/GitHub/DAO2/my-app3/src/components/step-four.jsx";
 
 import '/Applications/GitHub/DAO2/my-app3/src/app.css'
 
 const Profile = () => {
   
   const steps = [
-    { name: "Name A", component: <StepOne /> },
-    { name: "Email", component: <StepTwo /> },
-    { name: "Password", component: <StepThree /> },
-    { name: "Agreement", component: <StepFour /> }
+    { name: "ID", component: <StepOne /> },
+    { name: "Selfie", component: <StepTwo /> },
+    { name: "Connect", component: <StepThree /> },
+    //{ name: "Agreement", component: <StepFour /> }
   ];
     
     
